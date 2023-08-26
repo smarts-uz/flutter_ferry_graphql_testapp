f91673700dda390ad8e28faa9f1e5867adee825c
JasurbekErgashev <jergashev633@gmail.com>
2023-08-26 10:01:37 +0500

    get a supabase graphql schema

--- lib/main.dart
+++ lib/main.dart
@@ -9,3 +9,8 @@ void main() async {
   GetIt.I.registerLazySingleton<TypedLink>(() => client);
   runApp(const MyApp());
 }
+
+// get-graphql-schema https://qutzykcdiptuzoqspbte.supabase.co/graphql/v1 \
+//   --header 'Authorization=Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF1dHp5a2NkaXB0dXpvcXNwYnRlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTEwMzg3OTMsImV4cCI6MjAwNjYxNDc5M30.3mh_usRMxOnqk2-cbEVBjNVfyxgCAl_IdjoSa9eDhHg' \
+//   --header 'apikey=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF1dHp5a2NkaXB0dXpvcXNwYnRlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTEwMzg3OTMsImV4cCI6MjAwNjYxNDc5M30.3mh_usRMxOnqk2-cbEVBjNVfyxgCAl_IdjoSa9eDhHg' \
+//   > lib/schema.graphql
